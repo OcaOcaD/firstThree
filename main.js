55e2a777f0d716ab182447dec04e89480d6ca0ab
Luis Donaldo García Castro <luisdonaldogarciacastro@gmail.com>
2019-09-21 10:44:25 -0500

    function moved

--- main.js
+++ main.js
@@ -4,22 +4,7 @@ const renderer = new THREE.WebGLRenderer({ antialias: true})
 //My cubes array;
 var cubes = [];
 var different = [];
-createCube = (w, h, d, c) => {
-    renderer.setSize( window.innerWidth, window.innerHeight )
-    document.body.appendChild( renderer.domElement )
-    //Creating the cube
-    let geometry = new THREE.BoxGeometry( w, h, d);
-    let material = new THREE.MeshBasicMaterial( {
-        color: "#dadada", wireframe: true, transparent: true
-       })
-    let cube = new THREE.Mesh ( geometry, material )
-    scene.add( cube );
-    renderer.render( scene, camera );
-    // camera.position.z = 5;
-    // cube.position( 5,5,5 );
-    different.push(cube);
-    // camera.position.x = 3;
-}
+
 
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
--- threeThings.js
+++ threeThings.js
@@ -1,3 +1,19 @@
+createCube = (w, h, d, c) => {
+    renderer.setSize( window.innerWidth, window.innerHeight )
+    document.body.appendChild( renderer.domElement )
+    //Creating the cube
+    let geometry = new THREE.BoxGeometry( w, h, d);
+    let material = new THREE.MeshBasicMaterial( {
+        color: "#dadada", wireframe: true, transparent: true
+       })
+    let cube = new THREE.Mesh ( geometry, material )
+    scene.add( cube );
+    renderer.render( scene, camera );
+    // camera.position.z = 5;
+    // cube.position( 5,5,5 );
+    different.push(cube);
+    // camera.position.x = 3;
+}
 animate = () => {
 
     for (const qbe of cubes) {
@@ -10,9 +26,5 @@ animate = () => {
         }
         renderer.render( scene, camera )        
     }
-
-}
-animate_d = () => {
-    console.log(different);
 
 }
